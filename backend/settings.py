e4c684f9a107fa44968685b20bb72fb51e6c7df0
Syed Nameer Ali <s.nameer18606@gmail.com>
2025-08-15 02:07:59 +0500

    updated docker-compose

--- backend/settings.py
+++ backend/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
